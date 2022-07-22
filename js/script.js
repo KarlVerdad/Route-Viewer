bb0f633a2e62ae5fc458e1dd823f2156f9ad0b9e
KarlVerdad <karlfizz1999@gmail.com>
2022-07-22 10:58:29 +0800

    v0.2: Route Viewer
    - Added view function
      -> Points in the route can be viewed using UI buttons

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+let globalPath = [];
+let globalInd = 0;
+
 function init() {
   console.log("Initializing");
 
@@ -5,6 +8,7 @@ function init() {
   const mapOptions = {
     center: { lat: 14.6091, lng: 121.0223 },
     zoom: 7,
+    streetViewControl: false,
   };
   const map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
@@ -28,6 +32,10 @@ function init() {
     calcRoute(directionsService, directionsRenderer);
   };
   document.getElementById("calculate").onclick = onChangeHandler;
+  
+  // Interface
+  document.getElementById("view_next").onclick = view_next;
+  document.getElementById("view_prev").onclick = view_prev;
 }
 
 function calcRoute(service, renderer) {
@@ -45,61 +53,104 @@ function calcRoute(service, renderer) {
       const route = result.routes[0];
       let fullPath = getFullPath(route);
       console.log(fullPath);
+
+      globalPath = fullPath;
+      view(0);
+
     } else {
       alert("Route Invalid");
     }
   });
 }
 
-// TODO: Returns a straight path with a minimized number of waypoints
-function straightenPath(path) {
-  let newPath;
-  let angle = null;
-
-  for (let i = 0; i < path.length; i++) {
-    if (i == path.length - 1) {
-      break;
-    }
-    let p1 = toPoint(path[i].toUrlValue(4));
-    let p2 = toPoint(path[i + 1].toUrlValue(4));
-    if (p1.x == p2.x && p1.y == p2.y) {
-      continue;
-    }
-
-    console.log(i, p1, p2);
-    let newAngle = calcAngleDeg(p1, p2);
-    console.log(newAngle);
-  }
-}
-
-function calcAngleDeg(p1, p2) {
-  return (Math.atan2(p2.y - p1.y, p2.x - p1.x) * 180) / Math.PI;
-}
-
-function toPoint(latlng) {
+function toCoord(latlng) {
   let split = latlng.split(",");
   return {
-    x: split[1],
-    y: split[0],
+    lat: Number(split[0]),
+    lng: Number(split[1]),
   };
 }
 
+// Uses Geometry interpolation (Not set to use roads)
+function getFullPath2(route) {
+  let fullPath = [];
+
+  route.legs.forEach(function (leg) {
+    leg.steps.forEach(function (step) {
+      const start = step.start_location;
+      const end = step.end_location;
+      for (let i = 0; i < 10; i++) {
+        const p = toCoord(google.maps.geometry.spherical.interpolate(start, end, i * 0.1).toUrlValue(4));
+
+        if (fullPath.length > 0) {
+          const prev = fullPath[fullPath.length - 1].point;
+          if (p.lat == prev.lat && p.lng == prev.lng) {
+            return;
+          }
+
+          const a = google.maps.geometry.spherical.computeHeading(prev, p);
+          fullPath[fullPath.length - 1].angle = a;
+        }
+        
+        fullPath.push({point: p, angle: 0});
+      }
+    });
+  });
+
+  fullPath[fullPath.length - 1].angle = fullPath[fullPath.length - 2].angle;
+
+  return fullPath
+}
+
+// TODO: Collect all points first then...
+// TODO: Create custom interpolate function to minimize the path
 function getFullPath(route) {
-  const firstPoint = toPoint(route.legs[0].steps[0].path[0].toUrlValue(4));
-  let fullPath = [firstPoint];
+  const firstPoint = toCoord(route.legs[0].steps[0].path[0].toUrlValue(4));  
+  let fullPath = [{point: firstPoint, angle: 0}];
 
   route.legs.forEach(function (leg) {
     leg.steps.forEach(function (step) {
       step.path.forEach(function (point) {
-        let p = toPoint(point.toUrlValue(4));
-        let prev = fullPath[fullPath.length - 1];
-        if (p.x == prev.x && p.y == prev.y) {
+        const p = toCoord(point.toUrlValue(4));
+        const prev = fullPath[fullPath.length - 1].point;
+        if (p.lat == prev.lat && p.lng == prev.lng) {
           return;
         }
-        fullPath.push(p);
+        const a = google.maps.geometry.spherical.computeHeading(prev, p);
+        fullPath[fullPath.length - 1].angle = a;
+
+        fullPath.push({point: p, angle: 0});
       });
     });
   });
 
+  fullPath[fullPath.length - 1].angle = fullPath[fullPath.length - 2].angle;
+
   return fullPath
+}
+
+function clamp(num, min, max) {
+  return Math.min(Math.max(num, min), max);
+}
+
+function view(index) {
+  index = clamp(index, 0, globalPath.length - 1);
+  let panoSettings = {
+    position: globalPath[index].point,
+    pov: {heading: globalPath[index].angle, pitch: 0},
+    clickToGo: false,
+    scrollwheel: false,
+    linksControl: false,
+  }
+  const pano = new google.maps.StreetViewPanorama(document.getElementById("view"), panoSettings);
+  console.log(panoSettings.position, panoSettings.pov)
+  globalInd = index;
+}
+
+function view_next() {
+  view(globalInd + 1);
+}
+
+function view_prev() {
+  view(globalInd - 1);
 }
