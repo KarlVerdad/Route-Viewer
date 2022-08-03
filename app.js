9450f2f9c65879bd8e00cbeaaa83650012cc1758
KarlVerdad <karlfizz1999@gmail.com>
2022-08-03 17:26:10 +0800

    v0.7b: Node JS Integration (Bug Fix)
    - Changed port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.listen(3000);
+app.listen(process.env.PORT || 8080);
 
 app.use(express.static('static'));
 
